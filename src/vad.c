9cf94e7dc3b3ea1d549fd3f97d3551a5d9c94ae6
Pau PAV ETSETB <pau.pav@upc.edu>
2020-03-11 09:09:21 +0100

    Ficheros Practica 2

--- src/vad.c
+++ src/vad.c
@@ -13,15 +13,16 @@ const float FRAME_TIME = 10.0F; /* in ms. */
  */
 
 const char *state_str[] = {
-  "UNDEF", "S", "V", "INIT"
-};
+    "UNDEF", "S", "V", "INIT"};
 
-const char *state2str(VAD_STATE st) {
+const char *state2str(VAD_STATE st)
+{
   return state_str[st];
 }
 
 /* Define a datatype with interesting features */
-typedef struct {
+typedef struct
+{
   float zcr;
   float p;
   float am;
@@ -31,7 +32,13 @@ typedef struct {
  * TODO: Delete and use your own features!
  */
 
-Features compute_features(const float *x, int N) {
+Features compute_features(const float *x, int N, float fm)
+{
+  Features feat;
+  feat.p = compute_power(x, N);
+  feat.zcr = compute_zcr(x, N, fm);
+  feat.am = compute_am(x, N);
+
   /*
    * Input: x[i] : i=0 .... N-1 
    * Ouput: computed features
@@ -41,8 +48,7 @@ Features compute_features(const float *x, int N) {
    *
    * For the moment, compute random value between 0 and 1 
    */
-  Features feat;
-  feat.zcr = feat.p = feat.am = (float) rand()/RAND_MAX;
+
   return feat;
 }
 
@@ -50,7 +56,8 @@ Features compute_features(const float *x, int N) {
  * TODO: Init the values of vad_data
  */
 
-VAD_DATA * vad_open(float rate) {
+VAD_DATA *vad_open(float rate)
+{
   VAD_DATA *vad_data = malloc(sizeof(VAD_DATA));
   vad_data->state = ST_INIT;
   vad_data->sampling_rate = rate;
@@ -58,17 +65,20 @@ VAD_DATA * vad_open(float rate) {
   return vad_data;
 }
 
-VAD_STATE vad_close(VAD_DATA *vad_data) {
+VAD_STATE vad_close(VAD_DATA *vad_data)
+{
   /* 
    * TODO: decide what to do with the last undecided frames
    */
+
   VAD_STATE state = vad_data->state;
 
   free(vad_data);
   return state;
 }
 
-unsigned int vad_frame_size(VAD_DATA *vad_data) {
+unsigned int vad_frame_size(VAD_DATA *vad_data)
+{
   return vad_data->frame_length;
 }
 
@@ -77,7 +87,8 @@ unsigned int vad_frame_size(VAD_DATA *vad_data) {
  * using a Finite State Automata
  */
 
-VAD_STATE vad(VAD_DATA *vad_data, float *x) {
+VAD_STATE vad(VAD_DATA *vad_data, float *x)
+{
 
   /* 
    * TODO: You can change this, using your own features,
@@ -87,18 +98,29 @@ VAD_STATE vad(VAD_DATA *vad_data, float *x) {
   Features f = compute_features(x, vad_data->frame_length);
   vad_data->last_feature = f.p; /* save feature, in case you want to show */
 
-  switch (vad_data->state) {
+  switch (vad_data->state)
+  {
   case ST_INIT:
     vad_data->state = ST_SILENCE;
     break;
 
   case ST_SILENCE:
-    if (f.p > 0.95)
+    if (f.p > 30)
+      vad_data->state = ST_MaybeVOICE;
+    break;
+
+  case ST_MaybeVOICE:
+    if (f.p > 35)
       vad_data->state = ST_VOICE;
     break;
 
   case ST_VOICE:
-    if (f.p < 0.01)
+    if (f.p < 25)
+      vad_data->state = ST_MaybeSILENCE;
+    break;
+
+  case ST_MaybeSILENCE:
+    if (f.p < 20)
       vad_data->state = ST_SILENCE;
     break;
 
@@ -107,12 +129,14 @@ VAD_STATE vad(VAD_DATA *vad_data, float *x) {
   }
 
   if (vad_data->state == ST_SILENCE ||
-      vad_data->state == ST_VOICE)
+      vad_data->state == ST_VOICE || vad_data->state == ST_MaybeSILENCE || vad_data->state == ST_MaybeVOICE)
     return vad_data->state;
   else
     return ST_UNDEF;
 }
 
-void vad_show_state(const VAD_DATA *vad_data, FILE *out) {
+void vad_show_state(const VAD_DATA *vad_data, FILE *out)
+{
   fprintf(out, "%d\t%f\n", vad_data->state, vad_data->last_feature);
 }
+
